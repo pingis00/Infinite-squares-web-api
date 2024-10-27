4006cc8c33432706b169d1cadb58a0426dc0e8b0
Andreas <pingis00@hotmail.com>
2024-10-27 20:08:12 +0100

    Add BaseRepository with placeholder methods

--- InfiniteSquaresInfrastructure/Repositories/BaseRepository.cs
+++ InfiniteSquaresInfrastructure/Repositories/BaseRepository.cs
@@ -1,7 +1,33 @@
 ﻿using InfiniteSquaresCore.Interfaces.Repositories;
+using InfiniteSquaresCore.Responses;
+using System.Linq.Expressions;
 
 namespace InfiniteSquaresInfrastructure.Repositories;
 
 public class BaseRepository<T> : IBaseRepository<T> where T : class
 {
+    public Task<ResponseResult> CreateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseResult> DeleteAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseResult<IEnumerable<T>>> GetAllAsync(Expression<Func<T, bool>>? predicate = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseResult<T>> GetOneAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseResult> UpdateAsync(Expression<Func<T, bool>> predicate, T updatedEntity)
+    {
+        throw new NotImplementedException();
+    }
 }
