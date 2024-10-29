a72e77d061c839a24191626639ab455a5f53305a
Andreas <pingis00@hotmail.com>
2024-10-29 08:05:58 +0100

    refactor: centralize and improve application configuration

--- InfiniteSquaresWebAPI/Program.cs
+++ InfiniteSquaresWebAPI/Program.cs
@@ -3,15 +3,7 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAllOrigins",
-        policy => policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-
-});
-
+// Configure Serilog
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
@@ -19,25 +11,18 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Register services
 builder.Services.RegisterSwagger();
 builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
+// Configure middleware pipeline
 app.UseCors("AllowAllOrigins");
-
 app.UseSwagger();
 app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Infinite Squares API v1"));
-
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
