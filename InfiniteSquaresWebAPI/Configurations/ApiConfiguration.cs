f6f022583055fb76fd732bc22060996bffc253c7
Andreas <pingis00@hotmail.com>
2024-10-29 15:17:03 +0100

    "feat: add API middleware configuration and error handling

---
+++ InfiniteSquaresWebAPI/Configurations/ApiConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace InfiniteSquaresWebAPI.Configurations;
+
+public static class ApiConfiguration
+{
+    public static IServiceCollection AddApiConfigurations(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+        });
+
+        services.AddRateLimiter(options =>
+        {
+            options.AddFixedWindowLimiter("fixed", options =>
+            {
+                options.PermitLimit = 100;
+                options.Window = TimeSpan.FromMinutes(1);
+                options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                options.QueueLimit = 2;
+            });
+        });
+
+        services.AddHealthChecks();
+
+        return services;
+    }
+}
